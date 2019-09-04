a111954e480824c55b20ba62957c84b48987407e
hatef <hatef.alpoor@outlook.com>
2019-09-04 12:08:29 +0430

    util completed, can use bijenkhan corpus

--- attention.py
+++ attention.py
@@ -1,3 +1,7 @@
-print('hello world!')
+from util import Bijenkhan
 
-print('hatef alipoor')
+bijenkhan = Bijenkhan('/home/hatef/courses/Term-2/NLP/HWs/CA4/train_simple.txt')
+gen = bijenkhan.sent_tag_gen(100)
+s, t = next(gen)
+print(bijenkhan.get_vocab())
+print(bijenkhan.get_tags())
